55aa2cad48e3ae664a85421240c20323382d30ba
k023g1023@g.neec.ac.jp <k023g1023@g.neec.ac.jp>
2023-10-20 12:38:54 +0900

    Pullreqyest branch changed

--- PM1 プロジェクトテスト１.cpp
+++ PM1 プロジェクトテスト１.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::cout << "Hello VisualStdio Git!\n";
-    std::cout << "Hello New branch\n";
+    std::cout << "PullRequest branch changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
